28e5ed9c89f85637b940acf7882516ee44c5d708
yls-xian <yls4314300@163.com>
2021-03-02 15:59:21 +0800

    test

---
+++ src/java/test.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * @author yulinsheng
+ * @time 2021-03-02-15:54
+ * @email yls4314300@163.com
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
