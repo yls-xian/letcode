c52f59d35f8ad4c9c8837a9851da713c3d6ccbd4
yls-xian <yls4314300@163.com>
2021-03-08 09:54:26 +0800

    3.8周一合集

---
+++ src/letcodeBase/chain/ListNode.java
@@ -0,0 +1,17 @@
+package letcodeBase.chain;
+
+/**
+ * @author yulinsheng
+ * @time 2021-03-05-11:08
+ * @email yls4314300@163.com
+ */
+public class ListNode{
+//    定义节点的值和下一个节点的信息
+    int val;
+    ListNode next;
+    public ListNode(int val,ListNode next)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/letcodeBase/test.java
@@ -0,0 +1,9 @@
+package letcodeBase;
+
+/**
+ * @author yulinsheng
+ * @time 2021-03-05-11:03
+ * @email yls4314300@163.com
+ */
+public class test {
+}
