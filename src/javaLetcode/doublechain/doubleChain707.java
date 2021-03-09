b6cb2bfa5b72db747ef4deea53dcea5f23949271
yls-xian <yls4314300@163.com>
2021-03-09 22:23:33 +0800

    3.9周二(双链表操作)

---
+++ src/javaLetcode/doublechain/doubleChain707.java
@@ -0,0 +1,132 @@
+package javaLetcode.doublechain;
+
+/**
+ * @author yulinsheng
+ * @time 2021-03-09-16:06
+ * @email yls4314300@163.com
+ */
+public class doubleChain707 {
+}
+
+
+class MyLinkedList {
+    //        定义链表的大小
+    int size;
+    //        定义链表的头和尾
+    ListNode head,tail;
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+//        初始化链表的长度
+        size = 0;
+//        初始化头尾节点
+        head = new ListNode(0);
+        tail = new ListNode(0);
+//        连接head和tail
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        if(index >= size || index < 0){return -1;}
+        ListNode  curr = head;
+        if(index < size - index ){
+            for(int i = 0;i <= index;i++){
+
+                curr = curr.next;
+            }
+        }
+        else{
+            curr = tail;
+            for(int i = 0;i <= size - index ;i++){
+
+                curr = curr.pre;
+            }
+
+        }
+        return curr.val;
+
+    }
+
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+
+        addAtIndex(0,val);
+
+    }
+
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+
+
+    }
+
+
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if(index > size){return;}
+        if(index < 0){index = 0;}
+        ListNode pre,succ;
+//        由于是双链表,,因此我们需要考虑如何插入元素最快
+//        可以正向插入，也可以反向插入
+        if(index < size - index){
+//            靠近左边，正向插入元素
+            pre = head;
+            for(int i = 0;i < index;i++){
+                pre = pre.next;
+            }
+//            确定插入的元素的节点位置
+            succ = pre.next;
+        }
+//        靠近尾节点时，反向插入元素
+        else{
+            succ = tail;
+            for(int i = 0;i < size - index ;i++)
+            {
+                succ = succ.pre;
+            }
+            pre = succ.pre;
+        }
+//        插入元素后，整个长度+1
+        ++size;
+//        定义要插入的节点
+        ListNode toAdd = new ListNode(val);
+//        节点的前指针是pre
+        toAdd.pre = pre;
+//        节点的后指针是succ
+        toAdd.next = succ;
+        pre.next = toAdd;
+        succ.pre = toAdd;
+
+        }
+
+
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index > size){return;}
+        ListNode pred,succ;
+        if(index < size - index ){
+            pred = head;
+
+            for(int i = 0;i < index;i++) {
+                pred = pred.next;
+            }
+            succ = pred.next.next;
+        }
+        else{
+                succ = tail;
+            for(int i = 0;i < size - index;i++){
+                succ = succ.pre;
+            }
+            pred = succ.pre.pre;
+
+        }
+        size--;
+        pred.next = succ;
+        succ.pre = pred;
+
+
+
+    }
+}
---
+++ src/javaLetcode/doublechain/doubleChainOffer35.java
@@ -0,0 +1,72 @@
+package javaLetcode.doublechain;
+
+/**
+ * @author yulinsheng
+ * @time 2021-03-09-15:45
+ * @email yls4314300@163.com
+ */
+public class doubleChainOffer35 {
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+class SolutionOffer35 {
+    public Node copyRandomList(Node head) {
+        // 第一种情况，头节点为空，返回Null
+        if(head == null){
+            return null;
+        }
+        // 建立一个新的节点指向头节点->循环读取原始的节点数据
+        Node curNode = head;
+        while(curNode != null){
+            // 循环读取节点的数据，并且建立新的节点
+            Node tempNode = new Node(curNode.val);
+            // 将新的节点写入到原始的链表中
+            // 1->2  加入节点1    首先将要加入的节点1的next变成2
+            // 然后原始节点中1的next变成当前加入节点1
+            // 然后当前指针到达了2重复进行复制操作
+            tempNode.next = curNode.next;
+            curNode.next = tempNode;
+            curNode = tempNode.next;
+        }
+
+        // 重新回到头节点
+        curNode = head;
+        while(curNode != null){
+            if(curNode.random != null){
+                // 写入上述新节点的random指针
+                // 当前原始节点的下一个的random
+                // 指向原始节点的random的下一个（新的写入节点）
+                curNode.next.random = curNode.random.next;}
+            // 读取下一个原始节点
+            curNode = curNode.next.next;
+
+        }
+        // 拆分两种节点
+        // 重新回到原始链表的节点
+        curNode = head.next;
+        Node preNode = head,resNode = head.next;
+        while(curNode.next!=null){
+            // 原始节点
+            preNode.next = preNode.next.next;
+            // 新的节点
+            curNode.next = curNode.next.next;
+            preNode = preNode.next;
+            curNode = curNode.next;
+        }
+        // 原始链表的尾节点的下一个为空
+        preNode.next = null;
+        return resNode;
+    }
+
+
+}
